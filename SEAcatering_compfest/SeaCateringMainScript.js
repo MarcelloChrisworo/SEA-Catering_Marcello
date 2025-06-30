39c8153cc651dfdc8512b86f679cfb7e95e163b7
MarcelloChrisworo <markenz.mdc@gmail.com>
2025-06-30 18:20:33 +0700

    first commit

---
+++ SEAcatering_compfest/SeaCateringMainScript.js
@@ -0,0 +1,15 @@
+function Bantuan() {
+    window.location.href = "BantuanPage.html";
+}
+function TentangKami() {
+    window.location.href = "TentangKamiPage.html";
+}
+function LayananLegalStanding() {
+    window.location.href = "legalhukum.html";
+}
+function LayananContract() {
+    window.location.href = "kontrakhukum.html";
+}
+function LayananKonsultasi() {
+    window.open("https://www.facebook.com/", "_blank");
+}
